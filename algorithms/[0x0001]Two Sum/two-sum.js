c62ef656d75d26a7e85163bdb2d8afcb4984fb88
Shawn OY <shawnoy@outlook.com>
2017-10-27 12:45:28 +0800

    Fix indent

--- algorithms/[0x0001]Two Sum/two-sum.js
+++ algorithms/[0x0001]Two Sum/two-sum.js
@@ -3,17 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
- const twoSum = function (nums, target) {
-   const map = new Map()
-   let complement
+const twoSum = function (nums, target) {
+  const map = new Map()
+  let complement
 
-   for (let index in nums) {
-     complement = target - nums[index]
+  for (let index in nums) {
+    complement = target - nums[index]
 
-     if (map.has(complement) && map.get(complement) !== index) {
-       return [index, map.get(complement)].map(Number)
-     }
+    if (map.has(complement) && map.get(complement) !== index) {
+      return [index, map.get(complement)].map(Number)
+    }
 
-     map.set(nums[index], index)
-   }
- }
+    map.set(nums[index], index)
+  }
+}
